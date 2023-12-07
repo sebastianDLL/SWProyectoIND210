8907d6b76c6a760dffb754dd66d3ad7c0e1d0982
Sebastian Delgadillo <delgadillosebastian75@gmail.com>
2023-12-07 14:14:47 -0400

    arreglado

--- Main/javascript/javascriptL.js
+++ Main/javascript/javascriptL.js
@@ -1,15 +1,15 @@
 function irEntropia(){
-    window.location.href = "/html/entropia.html";
+    window.location.href = "./html/entropia.html";
 }
 
 function irHomeostasis(){
-    window.location.href = "/html/homeostasis.html";
+    window.location.href = "./html/homeostasis.html";
 }
 
 function irFactibilidad(){
-    window.location.href = "/html/factibilidad.html";
+    window.location.href = "./html/factibilidad.html";
 }
 
 function irCredito(){
-    window.location.href = "/html/tomaDecisiones.html";
+    window.location.href = "./html/tomaDecisiones.html";
 }
